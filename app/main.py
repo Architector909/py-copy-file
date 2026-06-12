1d6c889abf9b4241b00410a039288acfd5ce8909
Architector <katusha2105@gmail.com>
2026-06-12 17:45:45 +0300

    copy file

--- app/main.py
+++ app/main.py
@@ -1,1 +1,9 @@
-# write your code here
+def copy_file(command: str) -> None:
+    _, source_file, target_file = command.split()
+
+    if source_file == target_file:
+        return
+
+    with open(source_file, "r", encoding="utf-8") as file_in, \
+         open(target_file, "w", encoding="utf-8") as file_out:
+        file_out.write(file_in.read())
